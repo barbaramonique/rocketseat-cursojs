b4cc9a8a68b895f51362c34a5a9d8ed01962ccf8
unknown <barbara_setter@saojose.sc.senai.br>
2019-06-17 12:27:48 -0300

    iniciamos a aula 2

--- mod 3/aula 2/2/todos.js
+++ mod 3/aula 2/2/todos.js
@@ -1,1 +1,10 @@
-alert("esta funcionando")
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button")
+
+console.log(listElement);
+console.log(inputElement);
+console.log(buttonElement);
+
+var todos = {'fazer café', 'beber gasolina',
+'jogar lixo no lixo'};
